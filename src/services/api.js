bc19aa94214407b6910bc4931559fad780b823da
Andre Nakashima <andreenakashima@gmail.com>
2020-03-04 12:52:29 -0300

    chore: add config of My Json Server
    Uses My Json Server to create a fake server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
+  baseURL:
+    'https://my-json-server.typicode.com/andreenakashima/api-rocketshoes',
 });
 
 export default api;
